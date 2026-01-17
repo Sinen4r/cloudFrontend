99e39f41e50be35765f23dc651a04e4276494e56
Sinen4r <153398440+Sinen4r@users.noreply.github.com>
2026-01-17 13:48:34 +0100

    first commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0', function() {
   console.log('Server listening on 0.0.0.0:' + PORT);
 });
